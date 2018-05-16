482a95ae770f43542fea8e06ea60fad8627a100c
Egor Krämer <ek067@hdm-stuttgart.de>
2018-05-16 17:07:58 +0200

    DBConnection and ContactMapper

---
+++ src/com/google/gwt/sample/itProjekt/server/ContactMapper.java
@@ -0,0 +1,206 @@
+package com.google.gwt.sample.itProjekt.server;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Vector;
+
+import com.google.gwt.sample.itProjekt.shared.bo.Contact;
+
+public class ContactMapper {
+	
+	//static weil Singleton. Einzige Instanz dieser Klasse
+	private static ContactMapper contactmapper = null;
+	
+	protected ContactMapper contactMapper() {
+		if (contactmapper == null){
+			contactmapper = new ContactMapper();
+		}
+		return contactmapper;
+		}
+	public Contact findByID(int cid){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT c_id, firstName, lastName, gender, U_ID From T_Contact where c_id ="+ cid + " order by C_ID");
+			if (rs.next()){
+				Contact c = new Contact();
+				c.setId(rs.getInt("c_id"));
+				c.setFirstname(rs.getString("firstName"));
+				c.setLastname(rs.getString("lastName"));
+				c.setSex(rs.getString("gender"));
+				// Besitzer soll als User Objekt weitergegeben werden? warum nicht einfach als ID int?
+				//c.setParticipant(rs.getInt("U_ID"));
+				return c;	
+			}
+		}
+		catch (SQLException e2){
+			e2.printStackTrace();
+			return null;
+		}
+		return null;
+	}
+	
+	public Vector<Contact> findAll(){
+Connection con = DBConnection.connection();
+Vector<Contact> result = new Vector<Contact>();
+		
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT c_id, firstName, lastName, gender, U_ID From T_Contact order by C_ID");
+			
+			while (rs.next()){
+				Contact c = new Contact();
+				c.setId(rs.getInt("c_id"));
+				c.setFirstname(rs.getString("firstName"));
+				c.setLastname(rs.getString("lastName"));
+				c.setSex(rs.getString("gender"));
+				// Besitzer soll als User Objekt weitergegeben werden? warum nicht einfach als ID int?
+				//c.setParticipant(rs.getInt("U_ID"));
+				result.addElement(c);
+			}		
+		}catch(SQLException e2){
+			e2.printStackTrace();
+		}
+		return result;
+	}
+	
+	public Contact findByName(String firstname, String lastname){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT c_id, firstName, lastName, gender, U_ID From T_Contact where firstName ="+ firstname+ "AND lastname=" + lastname + " order by C_ID");
+			if (rs.next()){
+				Contact c = new Contact();
+				c.setId(rs.getInt("c_id"));
+				c.setFirstname(rs.getString("firstName"));
+				c.setLastname(rs.getString("lastName"));
+				c.setSex(rs.getString("gender"));
+				// Besitzer soll als User Objekt weitergegeben werden? warum nicht einfach als ID int?
+				//c.setParticipant(rs.getInt("U_ID"));
+				return c;	
+			}
+		}
+		catch (SQLException e2){
+			e2.printStackTrace();
+			return null;
+		}
+		return null;
+	}
+	public Vector<Contact> findAllByUID(int uid){
+		Connection con = DBConnection.connection();
+		Vector<Contact> result = new Vector<Contact>();
+				
+				try{
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT c_id, firstName, lastName, gender, U_ID From T_Contact where u_id=" + uid+ " order by C_ID");
+					
+					while (rs.next()){
+						Contact c = new Contact();
+						c.setId(rs.getInt("c_id"));
+						c.setFirstname(rs.getString("firstName"));
+						c.setLastname(rs.getString("lastName"));
+						c.setSex(rs.getString("gender"));
+						// Besitzer soll als User Objekt weitergegeben werden? warum nicht einfach als ID int?
+						//c.setParticipant(rs.getInt("U_ID"));
+						result.addElement(c);
+					}		
+				}catch(SQLException e2){
+					e2.printStackTrace();
+				}
+				return result;
+			}
+	
+	public Contact insert(Contact c){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT MAX(c_id) AS maxcid From T_Contact");
+			if (rs.next()){
+				
+				c.setId(rs.getInt("maxcid")+1);
+				Statement stmt2 = con.createStatement();
+				stmt2.executeUpdate("INSERT INTO T_Contact (c_id, firstname, lastname, gender, create_date, mod_date, u_id)"
+				+ " VALUES ('"
+				+ c.getId() 
+				+ "', '" 
+				+ c.getFirstname() 
+				+ "', '" 
+				+ c.getLastname() 
+				+ "', '" + c.getSex() 
+				+ "', '" 
+				+ new Timestamp(System.currentTimeMillis())  
+				+ ", "
+				+ new Timestamp(System.currentTimeMillis()) 
+				+ ", " 
+				+ 123 
+				+ ")") ;
+						
+				return c;	
+				// Timestamp testen...
+				// placeholder für sowas wie: c.getOWner.getID())
+			}
+		}
+		catch (SQLException e2){
+			e2.printStackTrace();
+			return c;
+		}
+		return c;}
+	
+		public Contact update(Contact c){
+			Connection con = DBConnection.connection();
+			
+			try{
+				Statement stmt = con.createStatement();
+				stmt.executeUpdate("UPDATE T_CONTACT SET firstname ='"+c.getFirstname()+"', lastname ='" + c.getLastname()+ "', gender=" + c.getSex() +"', mod_date="
+						+ new java.sql.Timestamp(System.currentTimeMillis()) + ", u_id=" + 123 + " Where c_id =" + c.getId());
+			}
+		
+		catch (SQLException e2){
+			e2.printStackTrace();
+			return c;
+		}
+		return c;}
+		
+		public void delete (Contact c){
+Connection con = DBConnection.connection();
+			
+			try{
+				
+				Statement stmt = con.createStatement();
+				stmt.executeUpdate("DELETE FROM T_CONTACT WHERE C_ID =" +c.getId());
+			}
+		
+		catch (SQLException e2){
+			e2.printStackTrace();
+			
+		}
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
---
+++ src/com/google/gwt/sample/itProjekt/server/DBConnection.java
@@ -0,0 +1,50 @@
+package com.google.gwt.sample.itProjekt.server;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+
+public class DBConnection {
+
+   
+     
+    private static Connection con = null;
+
+   
+    private static String googleUrl = "jdbc:google:mysql://it-projekt-gruppe-10-203610:europe-west1:itprojectdb/itpdb?user=root&password=root";
+    private static String googleUrl2 = "jdbc:mysql://it-projekt-gruppe-10-203610:europe-west1:itprojektdb/itpdb";
+    private static String localUrl = "jdbc:mysql://127.0.0.1:3306/bankproject?user=demo&password=demo";
+
+    
+    public static Connection connection() {
+       
+        if (con == null) {
+            String url = null;
+            try {
+                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+                    
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
+                   
+                    Class.forName("com.mysql.jdbc.Driver");
+                    url = localUrl;
+                }
+                
+                con = DriverManager.getConnection(url);
+            } catch (Exception e) {
+                con = null;
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+   
+        return con;
+    }
+
+}
+
