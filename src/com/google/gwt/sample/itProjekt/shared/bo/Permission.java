11966e44fc7679a97000584df72b8a2dcd59f145
JanNoller <jn025@hdm-stuttgart.de>
2018-05-16 14:45:08 +0200

    Added functions to Permission class.

--- src/com/google/gwt/sample/itProjekt/shared/bo/Permission.java
+++ src/com/google/gwt/sample/itProjekt/shared/bo/Permission.java
@@ -2,6 +2,31 @@ package com.google.gwt.sample.itProjekt.shared.bo;
 
 public class Permission extends BusinessObject {
 
-	private String participant;
+	private User participant;
 	private boolean isowner;
+	private BusinessObject shareableobject;
+	
+	public User getParticipant() {
+		return participant;
+	}
+	
+	public void setParticipant(User participant) {
+		this.participant = participant;
+	}
+
+	public boolean isIsowner() {
+		return isowner;
+	}
+
+	public void setIsowner(boolean isowner) {
+		this.isowner = isowner;
+	}
+
+	public BusinessObject getShareableobject() {
+		return shareableobject;
+	}
+
+	public void setShareableobject(BusinessObject shareableobject) {
+		this.shareableobject = shareableobject;
+	}
 }
