0f58b318f8ec245d6b3b7f1eda87f917f5a22df3
KatrinZerfass <kz021@hdm-stuttgart.de>
2018-05-23 16:37:18 +0200

    added lock function to ContactForm.java

--- src/com/google/gwt/sample/itProjekt/client/ContactForm.java
+++ src/com/google/gwt/sample/itProjekt/client/ContactForm.java
@@ -1,9 +1,16 @@
 package com.google.gwt.sample.itProjekt.client;
 
+import java.util.Vector;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.sample.itProjekt.shared.bo.Contact;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -13,15 +20,60 @@ public class ContactForm extends VerticalPanel {
 	Contact contactToDisplay = null;
 	//tbd: treeviewmodel
 	
+	
+	//innere Klasse für LockButtons
+	public class LockButton extends PushButton{
+		
+		public LockButton() {
+			lockUnlocked.setPixelSize(50, 50);
+			lockLocked.setPixelSize(50, 50);
+			
+			this.getUpFace().setImage(lockUnlocked);
+			
+			this.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				if(!isLocked) {
+				changeSchlossToLocked();
+				}else changeSchlossToUnlocked();
+			}
+		});
+		}
+		private boolean isLocked = false;
+		Image lockUnlocked = new Image("schlossUnlocked.png");
+		Image lockLocked = new Image ("schlossLocked.png");
+		
+		
+		
+		public void setIsLocked(boolean b) {
+		}
+		
+		public void changeSchlossToLocked() {
+			this.getUpFace().setImage(lockLocked);
+			isLocked = true;
+			
+		}
+		public void changeSchlossToUnlocked() {
+			this.getUpFace().setImage(lockUnlocked);
+			isLocked = false;
+		}
+			
+		
+	}
+	
 	public void onLoad(){
 		
 		super.onLoad();
+		//einstellungen für den Schloss-Button
+	
 		
+		//allumfassende Tabelle für die Darstellung von Kontakten
 		FlexTable contactTable = new FlexTable();
 		this.add(contactTable);
 		
+		//ab hier immer zum Testen kopieren
 		//Nullte Zeile
-		//leer
+	
+		contactTable.setText(0, 0, "  ");
 		
 		//Erste Zeile
 		contactTable.getFlexCellFormatter().setColSpan(1, 1, 4);
@@ -42,27 +94,47 @@ public class ContactForm extends VerticalPanel {
 		//Dritte Zeile
 		Label birthdateLabel = new Label("Geburtsdatum: ");
 		Label birthdayLabel = new Label();
+		Label sexLabel = new Label("Geschlecht: ");
+		ListBox sexListBox = new ListBox();
+		sexListBox.addItem("männlich");
+		sexListBox.addItem("weiblich");
 		
 		contactTable.setWidget(3, 0, birthdateLabel);
 		contactTable.setWidget(3, 1, birthdayLabel);
+		contactTable.setWidget(3, 2, sexLabel);
+		contactTable.setWidget(3, 3, sexListBox);
+		
 		
 		//Vierte Zeile
-		contactTable.getFlexCellFormatter().setColSpan(4, 1, 3);
 		Label addressLabel = new Label("Anschrift: ");
 		contactTable.setWidget(4, 0, addressLabel);
 		
+		contactTable.getFlexCellFormatter().setColSpan(4, 1, 3);
 		FlexTable addressTable = new FlexTable();
-		addressTable.getFlexCellFormatter().setColSpan(0,0,2);
-		addressTable.getFlexCellFormatter().setColSpan(1,1,2);
+		contactTable.setWidget(4, 1, addressTable);
+		
+	//	addressTable.getFlexCellFormatter().setColSpan(0,0,2);
+	//	addressTable.getFlexCellFormatter().setColSpan(1,1,2);
 		
 		TextBox streetTextBox = new TextBox();
 		addressTable.setWidget(0, 0, streetTextBox);
 		TextBox houseNrTextBox = new TextBox();
-		addressTable.setWidget(0, 2, houseNrTextBox);
+		addressTable.setWidget(0, 1, houseNrTextBox);
 		TextBox plzTextBox = new TextBox();
 		addressTable.setWidget(1, 0, plzTextBox);
 		TextBox cityTextBox = new TextBox();
 		addressTable.setWidget(1, 1, cityTextBox);
+		
+		addressTable.getFlexCellFormatter().setRowSpan(0, 2, 2);
+		
+		addressTable.setWidget(0, 2, new LockButton());
+		
+//		//nur zum innere Rahmenlinien anzeigen lassen, zu Debug-Zwecken
+//		for (int i= 0; i<addressTable.getRowCount(); i++) {
+//			for (int a=0; a<addressTable.getCellCount(i); a++) {
+//				addressTable.getCellFormatter().addStyleName(i, a, "cellBordersGreen");
+//			}
+//		}
 		
 		//Fünfte Zeile
 		contactTable.getFlexCellFormatter().setColSpan(5, 1, 3);
@@ -70,7 +142,11 @@ public class ContactForm extends VerticalPanel {
 		contactTable.setWidget(5, 0, phoneNumbersLabel);
 		
 		FlexTable phoneNumbersTable = new FlexTable();
-		phoneNumbersTable.getFlexCellFormatter().setColSpan(0,1,2);
+//		phoneNumbersTable.getFlexCellFormatter().setColSpan(0,1,2);
+//		phoneNumbersTable.getFlexCellFormatter().setColSpan(1,1,2);
+//		phoneNumbersTable.getFlexCellFormatter().setColSpan(2,1,2);
+		
+		contactTable.setWidget(5, 1, phoneNumbersTable);
 		
 		Label mobileNrLabel = new Label("Mobil: ");
 		Label privateNrLabel = new Label("Privat: ");
@@ -83,13 +159,45 @@ public class ContactForm extends VerticalPanel {
 		TextBox privateNrTextBox = new TextBox();
 		TextBox businessNrTextBox = new TextBox();
 		phoneNumbersTable.setWidget(0, 1, mobileNrTextBox);
-		phoneNumbersTable.setWidget(1, 1, mobileNrTextBox);
-		phoneNumbersTable.setWidget(2, 1, mobileNrTextBox);
+		phoneNumbersTable.setWidget(1, 1, privateNrTextBox);
+		phoneNumbersTable.setWidget(2, 1, businessNrTextBox);
+		
+		phoneNumbersTable.setWidget(0, 2, new LockButton());
+		phoneNumbersTable.setWidget(1, 2, new LockButton());
+		phoneNumbersTable.setWidget(2, 2, new LockButton());
 
-		//to do next time: Hinzufügen-buttons hinter jede Eigenschaft!
-		// + Löschen-button hinter jede einzelne Ausprägung
-		// + Symbole vor jede einzelne Ausprägung für Schlösser
+//		//nur zum innere Rahmenlinien anzeigen lassen, zu Debug-Zwecken
+//		for (int i= 0; i<phoneNumbersTable.getRowCount(); i++) {
+//			for (int a=0; a<phoneNumbersTable.getCellCount(i); a++) {
+//				phoneNumbersTable.getCellFormatter().addStyleName(i, a, "cellBordersGreen");
+//			}
+//		}
+		
+		
+		//Fünfte Zeile: eMail-Adresse
+		
+		//Sechste Zeile: Arbeitsstelle
+		
+		//Siebte Zeile: Buttons
+		
+		//todo: Hinzufügen buttons + Clickhandler
+		
+		
+		//Innere Rahmenlinien markieren zu Debug-Zwecken
+		for (int i= 0; i<contactTable.getRowCount(); i++) {
+			for (int a=0; a<contactTable.getCellCount(i); a++) {
+				contactTable.getCellFormatter().addStyleName(i, a, "cellBordersBlack");
+			}
+		}
+		
+		
+		
+		
+		
+		
 	}
+		
+	
 	
 
 }
