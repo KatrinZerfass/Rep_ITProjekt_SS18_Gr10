fb6f42499450f96f4178ba9db6db4d95998be6d6
JoshuaHill <hilljoshi@gmail.com>
2018-05-30 13:32:29 +0200

    Fixing GitHub

---
+++ src/com/google/gwt/sample/itProjekt/shared/ReportGenerator.java
@@ -0,0 +1,45 @@
+package com.google.gwt.sample.itProjekt.shared;
+
+import java.util.Vector;
+
+import com.google.gwt.sample.itProjekt.server.db.ContactListMapper;
+import com.google.gwt.sample.itProjekt.server.db.ContactMapper;
+import com.google.gwt.sample.itProjekt.server.db.PropertyMapper;
+import com.google.gwt.sample.itProjekt.server.db.UserMapper;
+import com.google.gwt.sample.itProjekt.server.db.ValueMapper;
+
+import com.google.gwt.sample.itProjekt.shared.report.AllContactsReport;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class ReportGenerator extends RemoteServiceServlet{
+	
+	private UserMapper uMapper;
+	private ContactMapper cMapper;
+	private PropertyMapper pMapper;
+	private ValueMapper vMapper;
+	
+	public void init() throws IllegalArgumentException{
+		this.uMapper = UserMapper.userMapper();
+		this.cMapper = ContactMapper.contactMapper();
+		this.pMapper = PropertyMapper.propertyMapper();
+		this.vMapper = ValueMapper.valueMapper();
+		
+	}
+	
+	public AllContactsReport showAllContacts() throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public AllContactsOfUserReport showAllContactsOf(User u) {
+		AllContactsOfUserReport result=new AllContactsOfUserReport();
+	}
+	
+	public AllSharedContactsOfUserReport showAllSharedContactsWith(User u) {
+		
+	}
+	
+	public AllContactsWithValueReport showAllshowAllContactsWith(Value v) {
+		
+	}
+}
