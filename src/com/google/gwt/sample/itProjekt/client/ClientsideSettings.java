0fae648a9c030d2c227184d3dec2a2090d30dc4c
JoshuaHill <hilljoshi@gmail.com>
2018-06-03 16:13:54 +0200

    Added ClientsideSettings

---
+++ src/com/google/gwt/sample/itProjekt/client/ClientsideSettings.java
@@ -0,0 +1,37 @@
+package com.google.gwt.sample.itProjekt.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.sample.itProjekt.shared.EditorAdministration;
+import com.google.gwt.sample.itProjekt.shared.EditorAdministrationAsync;
+import com.google.gwt.sample.itProjekt.shared.ReportGenerator;
+import com.google.gwt.sample.itProjekt.shared.ReportGeneratorAsync;
+
+
+
+public class ClientsideSettings {
+	
+	
+	private static EditorAdministrationAsync editorAdministration = null;
+	private static ReportGeneratorAsync reportGenerator = null;
+	
+	
+	
+	
+	public static EditorAdministrationAsync getEditorAdministration() {
+	   
+	    if (editorAdministration == null) {
+	     editorAdministration = GWT.create(EditorAdministration.class);
+	    }
+	    return editorAdministration;
+	  }
+	
+	
+	
+	public static ReportGeneratorAsync getReportGenerator() {
+	    if (reportGenerator == null) {
+	      reportGenerator = GWT.create(ReportGenerator.class);
+	    }
+	    return reportGenerator;
+	  }
+
+}
