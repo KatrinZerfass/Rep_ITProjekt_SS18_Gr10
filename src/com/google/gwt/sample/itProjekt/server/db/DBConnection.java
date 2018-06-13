edad8507c3614296ac620d830e73d2cd0d5e8db3
Egor Krämer <ek067@hdm-stuttgart.de>
2018-06-13 13:42:01 +0200

    Next try DBCONNECT

--- src/com/google/gwt/sample/itProjekt/server/db/DBConnection.java
+++ src/com/google/gwt/sample/itProjekt/server/db/DBConnection.java
@@ -15,25 +15,23 @@ public class DBConnection {
     private static Connection con = null;
 
    
-    private static String googleUrl = "jdbc:google:mysql://it-projekt-gruppe-10-203610:europe-west1:itprojectdb/itpdb2?user=root&password=root";
-    private static String googleUrl2 = "jdbc:mysql://it-projekt-gruppe-10-203610:europe-west1:itprojektdb/itpdb2";
-    private static String googleUrl3 = "jdbc:google:mysql://$it-projekt-gruppe-10-203610:europe-west1:itprojektdb/$itpdb2?user=$root&amp;password=$root";
    
-    
-    //private static String localUrl = "jdbc:mysql://127.0.0.1:3306/bankproject?user=demo&password=demo";
-    private static String localUrl2 = "jdbc:mysql://35.233.24.130/it-projekt-gruppe-10-203610:europe-west1:itprojectdb/itpdb2?user=root&password=root";
-    
     public static Connection connection() {
        
         if (con == null) {
-            String url = null;
+            
             try {
                 if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
                     
                     Class.forName("com.mysql.jdbc.GoogleDriver");
-                    con = DriverManager.getConnection("jdbc:google:mysql://it-projekt-gruppe-10-203610:itprojektdb/itpdb2?user=root&password=root");
+                    Window.alert("gleich wirds spannend");
+                  //  con = DriverManager.getConnection("jdbc:google:mysql://it-projekt-gruppe-10-203610:itprojektdb/itpdb2?user=root&password=root");
+                    con = DriverManager.getConnection("jdbc:google:mysql://it-projekt-gruppe-10-203610:europe-west1:itprojektdb/itpdb2?user=root&password=root");
                     Window.alert("lief wohl durch hier");
                 } else {
+                	Class.forName("com.mysql.jdbc.Driver");
+                	Window.alert("gleich wirds spannend lokal");
+                	con = DriverManager.getConnection("jdbc:mysql://35.233.24.130/itpdb2", "root", "root");
                    Window.alert("da lief was schief");
                    
                 }
