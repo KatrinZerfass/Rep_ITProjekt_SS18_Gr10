a3abcd665f01683ca38741a92e25ac9fdfec3eb7
Anna-Maria Gmeiner <ag111@hdm-stuttgart.de>
2018-06-27 12:20:37 +0200

    had wrong Mapper function

--- src/com/google/gwt/sample/itProjekt/shared/EditorAdministrationAsync.java
+++ src/com/google/gwt/sample/itProjekt/shared/EditorAdministrationAsync.java
@@ -90,5 +90,5 @@ public interface EditorAdministrationAsync {
 
 	void getAllContactsOfUserWithValue(User user, Value value, AsyncCallback<Vector<Contact>> callback);
 
-	void getContactsByOfUserWithProperty(User user, Property Property, AsyncCallback<Vector<Contact>> callback);
+	void getContactsOfUserWithProperty(User user, Property Property, AsyncCallback<Vector<Contact>> callback);
 }
