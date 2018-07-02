631bff978570a665713fcdcc10222a7c58450e8b
Anna-Maria Gmeiner <ag111@hdm-stuttgart.de>
2018-07-02 10:31:16 +0200

    added Comments in ReportGenerator.

--- src/com/google/gwt/sample/itProjekt/client/ITProjekt_SS18_Gr_10_Report.java
+++ src/com/google/gwt/sample/itProjekt/client/ITProjekt_SS18_Gr_10_Report.java
@@ -33,53 +33,105 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.Window;
 
-/*
+
+/**
  *  Entry-Point-Klasse des Report Generators.
  *  Der Reportgenerator besteht aus einem Navigationsteil, einer Kontaktliste und
- *  einer Detailansicht  
+ *  einer Detailansicht 
  */
-
 public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 	
-	//Relevante Attribute für LoginService
+	/**Relevante Attribute für LoginService*/
 	
+	/** Das logininfo objekt. */
 	private LoginInfo loginInfo = null;
+	
+	/** Das VerticalPanel login panel. */
 	private VerticalPanel loginPanel = new VerticalPanel();
+	
+	/** Das login label. */
 	private Label loginLabel = new Label(
 	      "Please sign in to your Google Account to access the application.");
+	
+	/** Der signIn link. */
 	private Anchor signInLink = new Anchor("Sign In");
+	
+	/** Der sign out link. */
 	private Anchor signOutLink = new Anchor("Sign Out");
 	
 	
+	/** Das Hauptpanel "mainpanel". */
 	VerticalPanel mainPanel = new VerticalPanel ();
+	
+	/** Der Reportbutton panel. */
 	HorizontalPanel reportbuttonPanel=new HorizontalPanel();
+	
+	/** Ein weiteres Panel für die Suche. */
 	HorizontalPanel searchPanel=new HorizontalPanel();
+	
+	/** Das weitere Panel für die Beschreibung der Suchmöglichkeiten. */
 	HorizontalPanel descriptionPanel = new HorizontalPanel();
+	
+	/** Das Panel für die Suche nach bestimmten Eigenschaften. */
 	HorizontalPanel propertyPanel=new HorizontalPanel();
 	
+	/** Das Panel für die Ausprägungen und die geteilten Nutzer. */
 	HorizontalPanel addPanel = new HorizontalPanel();
 	
+	/** Das user objekt. */
 	private User user = null;
+	
+	/** Die asynchrone ReportGenerator Klasse. */
 	private ReportGeneratorAsync reportGenerator=null;
+	
+	/** Die SuggestBox für die Email eingabe. */
 	private TextSuggest sb=null;
 	
-	/*
-	 * Die notwendigen Buttons für den Navigationsteil 
-	 */
+	/** Das Label für die Beschreibung der Suchmöglichkeiten */
+
 	Label searchheading = new Label("Hier können Sie Ihre Kontakte gefiltert nach Eigenschaft oder Ausprägung ausgeben.");
+	
+	/** Das Label für das Suchfeld. */
 	Label searchLabel = new Label("Suche: ");
 	
+	/** Das Eingabefeld für die Eigenschaften, falls sonstiges ausgewählt wird. */
 	TextBox propertyInput = new TextBox();
+	
+	/** Die Auswahlbox der vordefinierten Eigenschaften. */
 	ListBox propertylistbox = new ListBox();
+	/**
+	 * Die notwendigen Buttons für den Navigationsteil 
+	 **/
+	
+	/** Der Button, für den Report von allen Kontakten des angemeldeten Nutzers. */
 	Button allContactsOfUserButton = new Button("Alle Kontakte eines Nutzers");
+	
+	/** Der Button, für den Report von allen geteilten Kontakten des Nutzers mit einem bestimmten anderen Kontakt. */
 	Button allSharedContactsOfUserButton = new Button("Alle geteilten Kontakte eines Nutzers");
+	
+	/** Der Button, für den Report von allen Kontakten des Nutzers mit einer bestimmten Ausprägung. */
 	Button allContactsWithValueButton = new Button("Kontakte mit bestimmter Ausprägung");
+	
+	/** Der Button, für den Report von allen Kontakten des Nutzers, mit einer bestimmten Eigenschaft. */
 	Button allContactsWithPropertyButton = new Button("Kontakte mit bestimmter Eigenschaft");
 
+	/**
+	 * Die innere Klasse TextSuggest, dient dazu, dass man alle angemeldeten Nutzer 
+	 * in einer SuggestBox vorgeschlagen bekommt.
+	 */
 	public class TextSuggest{
 
+		/** Die Deklaration der SuggestBox. */
 		private SuggestBox sb;
+        
+        /** Die Deklaration vom MultiWordSuggestOracle oracle. */
         private MultiWordSuggestOracle oracle;
+        
+        /**
+         * Konstruktor für die TextSuggest Klasse.
+         *
+         * @param inputOracle the input oracle
+         */
         public TextSuggest(MultiWordSuggestOracle inputOracle) {
     		
     		setOracle(inputOracle);
@@ -102,18 +154,38 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
     		});
     	}		
 
+		/**
+		 * Getter der Suggest box.
+		 *
+		 * @return the suggest box
+		 */
 		public SuggestBox getSuggestBox() {
 			return sb;
 		}
 
+		/**
+		 * Setter der Suggest box.
+		 *
+		 * @param sb the new suggest box
+		 */
 		public void setSuggestBox(SuggestBox sb) {
 			this.sb = sb;
 		}
 
+		/**
+		 * Getter für die MultiWordSuggestOracle.
+		 *
+		 * @return the oracle
+		 */
 		public MultiWordSuggestOracle getOracle() {
 			return oracle;
 		}
 
+		/**
+		 * Setter für die MultiWordSuggestOracle.
+		 *
+		 * @param oracle the new oracle
+		 */
 		public void setOracle(MultiWordSuggestOracle oracle) {
 			this.oracle = oracle;
 		}
@@ -121,15 +193,23 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 
 			
 	
+	/**
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 * Die onModuleLoad() Methode. Hier werden alle Styles, der ChangeHandler 
+	 * und der LoginService hinzugefügt.   
+	**/
 	public void onModuleLoad() {
 		
 		reportGenerator=ClientsideSettings.getReportGenerator();
 		RootPanel.get("reporttext").setVisible(false);
+		/**
+		 * zusätzliche Klassennamen, die für das stylesheet benötigt werden.
+		**/
 		searchheading.addStyleName("searchheading");
 		signOutLink.addStyleName("signout");
 		signInLink.addStyleName("reportbutton");
-		sb=new TextSuggest(new MultiWordSuggestOracle());
-		
+		sb.getSuggestBox().addStyleName("reportSuggestBox");
+
 		searchLabel.addStyleName("searchlabel");
 		reportbuttonPanel.addStyleName("top-buttons");
 		allContactsOfUserButton.addStyleName("reportbutton");
@@ -137,6 +217,16 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 		allContactsWithValueButton.addStyleName("reportbutton");
 		allContactsWithPropertyButton.addStyleName("reportbutton");
 		propertyInput.addStyleName("sonstigeinput");
+		
+		/**
+		 * Instanziierung der SuggestBox für die Eingabe der Email.
+		**/
+		sb=new TextSuggest(new MultiWordSuggestOracle());
+		
+		/**
+		 * Auslesen der vordefinierten Eigenschaften aus der Datenbank, um diese zur ListBox hinzuzufügen.
+		**/
+		
 		reportGenerator.getAllPredefinedPropertiesOfReport(new AsyncCallback<Vector<Property>>(){
 			public void onFailure(Throwable t) {
 				Window.alert("Auslesen aller vordefinierten Eigenschaften fehlgeschlagen");
@@ -149,6 +239,11 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 			}
 		});
 		
+		/**
+		 * Hinzufügen eines ChangeHandler, um eine weitere TextBox darzustellen, 
+		 * falls die Eigenschaft Sonstiges ausgewählt wird.
+		**/
+		
 		propertylistbox.addChangeHandler(new ChangeHandler() {
             @Override
             public void onChange(ChangeEvent event) {
@@ -158,9 +253,12 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
             	if(Item==(size-1)){
             		propertyPanel.add(propertyInput);
                 	}
-            	
-
-            }});
+        }});
+		
+		/**
+		 * Hinzufügen des LoginServices
+		**/
+		
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
 	    loginService.login("https://it-projekt-gruppe-10-203610.appspot.com/ITProjekt_SS18_Gr_10_Report.html", new AsyncCallback<LoginInfo>() {
 	    	public void onFailure(Throwable error) {
@@ -176,6 +274,10 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 	    });
 	  }
 		
+	/**
+	 * Laden der Nutzerinformationen und das Speichern der Nutzerinformationen in einer Variable.
+	 */
+	
 	public void loadUserInformation() {
     	
 		if(reportGenerator == null){
@@ -198,20 +300,32 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 	    });
   	}
 	
-	 public void loadApplication() { 
+	 /**
+ 	 * Laden der Anwendung. In dieser Methode werden alle ClickHandler der Buttons instanziiert. 
+ 	 * Außerdem wird das RootPanel befüllt.  
+ 	 */
+ 	public void loadApplication() { 
 	  
-		 
-			signOutLink.setHref(loginInfo.getLogoutUrl());
-						
-			allContactsOfUserButton.addClickHandler(new ClickHandler() {
+	 		 /**
+	 	 	 * Hinzufügen des SignOut Links. 
+	 	 	 */
+			
+ 			signOutLink.setHref(loginInfo.getLogoutUrl());
+			
+			/**
+	 	 	 * Hinzufügen ClickHandlers für den AllContactsOfUser Button. Hier wird der Report mittels des
+	 	 	 * HTML ReportWriters erstellt. 
+	 	 	 */			
+			
+ 			allContactsOfUserButton.addClickHandler(new ClickHandler() {
 		         @Override
 		         public void onClick(ClickEvent event){
 		        	 reportGenerator.generateAllContactsOfUserReport(user, new AsyncCallback<AllContactsOfUserReport>() {
 	    						 public void onFailure(Throwable caught) {
-//	   				 				    RootPanel.get("reporttext").setVisible(false);	
-//	   				 				    Label errornote=new Label("Es Existieren leider keine Kontakte");
-//	   				 				    errornote.addStyleName("errornote");
-//	   				 				    mainPanel.add(errornote);
+	   				 				    RootPanel.get("reporttext").setVisible(false);	
+	   				 				    Label errornote=new Label("Es ist leider ein Fehler aufgetreten, versuchen Sie es später noch einmal.");
+	   				 				    errornote.addStyleName("errornote");
+	   				 				    mainPanel.add(errornote);
 	    							 
 	    						 }
 	    						 public void onSuccess(AllContactsOfUserReport result) {
@@ -230,7 +344,10 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 					 );
 		        	 
 		    
-	 
+ 			/**
+	 	 	 * Hinzufügen ClickHandlers für den allSharedContactsOfUserButton Button. Hier wird der Report mittels des
+	 	 	 * HTML ReportWriters erstellt und der Inhalt der SuggestBox ausgelesen. 
+	 	 	 */	
 			
 			allSharedContactsOfUserButton.addClickHandler(new ClickHandler() {
 				@Override
@@ -262,7 +379,10 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 	   				 	}
 					});
 				
-
+			/**
+	 	 	 * Hinzufügen ClickHandlers für den allContactsWithValueButton Button. Hier wird der Report mittels des
+	 	 	 * HTML ReportWriters erstellt und der Inhalt der SuggestBox ausgelesen. 
+	 	 	 */	
 			
 			allContactsWithValueButton.addClickHandler(new ClickHandler() {
 				@Override
@@ -289,6 +409,11 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 					Window.alert("Suchleiste ist leer. Bitte füllen Sie einen Suchbegriff in das Suchfeld ein.");
 					
 				}}});
+			
+			/**
+	 	 	 * Hinzufügen ClickHandlers für den allContactsWithPropertyButton Button. Hier wird der Report mittels des
+	 	 	 * HTML ReportWriters erstellt und die ausgewählte Eigenschaft aus der ListBox bzw. aus der TextBox ausgelesen. 
+	 	 	 */	
 			
 			allContactsWithPropertyButton.addClickHandler(new ClickHandler() {
 				@Override
@@ -338,10 +463,12 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 	   			}
 			});
 			
+			/**
+	 	 	 * Hinzufügen der einzelnen Panels zum Hauptpanel und anschließendes Hinzufügen des Hauptpanels zum RootPanel.
+	 	 	 */	
 			
 			descriptionPanel.add(searchheading);
 			addPanel.add(searchLabel);
-			sb.getSuggestBox().addStyleName("reportSuggestBox");
 			addPanel.add(sb.getSuggestBox());
 			reportbuttonPanel.add(allContactsOfUserButton);
 			addPanel.add(allSharedContactsOfUserButton);
@@ -356,25 +483,18 @@ public class ITProjekt_SS18_Gr_10_Report implements EntryPoint {
 			mainPanel.add(descriptionPanel);
 			mainPanel.add(addPanel);
 			mainPanel.add(propertyPanel);
-			RootPanel.get("report").add(mainPanel);
-			
-			  
+			RootPanel.get("report").add(mainPanel); 
 		  }
-	 
-	 /**
-	 * Der Konstruktor von InputDialogBox
-	 * ?? was macht er ??
-	 */
-				
-	
-
+	 	
+	/**
+	 * Load Login Methode, welches die LogIninfo aufruft und diese zum loginPanel hinzufügt. 
+	 * Anschließend wird dieses zum RootPanel, damit dies auf der HTML Seite dargestellt wird.
+	 */	
 	private void loadLogin() {
 		  
 		    signInLink.setHref(loginInfo.getLoginUrl());
 		    loginPanel.add(loginLabel);
 		    loginPanel.add(signInLink);
 		    RootPanel.get("loginRepo").add(loginPanel);
-		  }
-	 
-	 
+		  } 
 }
