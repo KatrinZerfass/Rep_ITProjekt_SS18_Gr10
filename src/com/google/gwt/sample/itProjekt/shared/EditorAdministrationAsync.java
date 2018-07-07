c1d8ae89ed8eb8e04a7fbbc1361248c24845ba3f
JanNoller <jn025@hdm-stuttgart.de>
2018-07-07 14:22:55 +0200

    Remade async interface.

--- src/com/google/gwt/sample/itProjekt/shared/EditorAdministrationAsync.java
+++ src/com/google/gwt/sample/itProjekt/shared/EditorAdministrationAsync.java
@@ -15,11 +15,18 @@ public interface EditorAdministrationAsync {
 
 	void addContactToContactList(ContactList contactlist, Contact contact, AsyncCallback<ContactList> callback);
 
+	void createAddress(String street, String housenumber, String zip, String city, Contact contact,
+			AsyncCallback<Value> callback);
+
 	void createContact(String firstname, String lastname, String sex, User user, AsyncCallback<Contact> callback);
 
 	void createContactList(String name, User user, AsyncCallback<ContactList> callback);
 
+	void createProperty(Contact contact, String type, AsyncCallback<Property> callback);
+
 	void createUser(String email, AsyncCallback<User> callback);
+
+	void createUserContact(String firstname, String lastname, String sex, String email, AsyncCallback<User> callback);
 
 	void createValue(Contact contact, int propertyid, String content, AsyncCallback<Value> callback);
 
@@ -27,30 +34,101 @@ public interface EditorAdministrationAsync {
 
 	void deleteContactList(ContactList contactlist, boolean owner, User user, AsyncCallback<Void> callback);
 
+	void deletePermission(User user, BusinessObject bo, AsyncCallback<Void> callback);
+
+	void deleteProperty(Property property, AsyncCallback<Void> callback);
+
+	void deleteUser(User u, AsyncCallback<Void> callback);
+
 	void deleteValue(Value value, AsyncCallback<Void> callback);
 
 	void editContact(int id, String firstname, String lastname, String sex, AsyncCallback<Contact> callback);
 
 	void editContactList(int id, String name, AsyncCallback<ContactList> callback);
 
+	void editPermissionContact(Permission permission, AsyncCallback<Permission> callback);
+
+	void editPermissionContactList(Permission permission, AsyncCallback<Permission> callback);
+
+	void editProperty(Property property, AsyncCallback<Property> callback);
+
+	void editUser(User user, AsyncCallback<User> callback);
+
 	void editValue(Contact contact, int propertyId, Value value, String content, boolean isshared,
 			AsyncCallback<Value> callback);
 
 	void getAllContactListsOfUser(String email, AsyncCallback<Vector<ContactList>> callback);
 
-	void getAllContactsOfContactList(ContactList contactlist, AsyncCallback<Vector<Contact>> callback);
+	void getAllContactListsWithContact(Contact contact, AsyncCallback<Vector<ContactList>> callback);
 
 	void getAllContactsOfActiveUser(User user, AsyncCallback<Vector<Contact>> callback);
 
+	void getAllContactsOfContactList(ContactList contactlist, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllContactsOfContactlistForUser(ContactList contactlist, User user,
+			AsyncCallback<Vector<Contact>> callback);
+
+	void getAllContactsOfUserWithValue(User user, Value value, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllContactsWithName(String name, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllContactsWithValue(String content, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllOwnedContactListsOfActiveUser(User user, AsyncCallback<Vector<ContactList>> callback);
+
 	void getAllOwnedContactsOfUser(String email, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllParticipantsOfContact(Contact contact, AsyncCallback<Vector<User>> callback);
+
+	void getAllParticipantsOfContactList(ContactList contactlist, AsyncCallback<Vector<User>> callback);
+
+	void getAllPermissions(AsyncCallback<Vector<Permission>> callback);
+
+	void getAllPredefinedPropertiesOf(AsyncCallback<Vector<Property>> callback);
+
+	void getAllSharedContactsOfContactList(ContactList contactlist, User user, AsyncCallback<Vector<Contact>> callback);
+
+	void getAllSharedContactsOfUserWithOtherUser(User source, String receiver, AsyncCallback<Vector<Contact>> callback);
 
 	void getAllSharedContactsWithUser(String email, AsyncCallback<Vector<Contact>> callback);
 
+	void getAllSharedValuesOfContact(Contact contact, AsyncCallback<Vector<Value>> callback);
+
+	void getAllUserSuggestions(User activeUser, AsyncCallback<Vector<String>> callback);
+
+	void getAllUsers(AsyncCallback<Vector<User>> callback);
+
+	void getAllValuesOfContact(Contact contact, AsyncCallback<Vector<Value>> callback);
+
 	void getContactByID(int id, AsyncCallback<Contact> callback);
 
-	void isUserKnown(String email, AsyncCallback<Boolean> callback);
+	void getContactsOfNameSearchResult(User user, String textBox, ContactList selectedContactList,
+			AsyncCallback<Vector<Contact>> callback);
+
+	void getContactsOfUserWithDefaultProperty(User user, Property property, AsyncCallback<Vector<Contact>> callback);
+
+	void getContactsOfUserWithProperty(User user, Property Property, AsyncCallback<Vector<Contact>> callback);
+
+	void getContactsOfValueSearchResult(User user, String textBox, ContactList selectedContactList,
+			AsyncCallback<Vector<Contact>> callback);
+
+	void getFullNamesOfUsers(Vector<User> user, AsyncCallback<Vector<String>> callback);
+
+	void getOwnerOfContact(Contact contact, AsyncCallback<User> callback);
+
+	void getPropertyByType(String type, Contact contact, AsyncCallback<Property> callback);
+
+	void getPropertyOfValue(Value value, AsyncCallback<Property> callback);
+
+	void getSourceToSharedContact(Contact contact, User receivingUser, AsyncCallback<User> callback);
+
+	void getUserByEmail(String email, AsyncCallback<User> callback);
+
+	void getUserByID(int ID, AsyncCallback<User> callback);
 
 	void init(AsyncCallback<Void> callback);
+
+	void isUserKnown(String email, AsyncCallback<Boolean> callback);
 
 	void removeContactFromContactList(ContactList contactlist, Contact contact, AsyncCallback<ContactList> callback);
 
@@ -59,83 +137,4 @@ public interface EditorAdministrationAsync {
 	void shareContactList(User sourceUser, String shareUserEmail, ContactList shareContactList,
 			AsyncCallback<Permission> callback);
 
-	void getAllOwnedContactListsOfActiveUser(User user, AsyncCallback<Vector<ContactList>> callback);
-
-	void getAllValuesOfContact(Contact contact, AsyncCallback<Vector<Value>> callback);
-
-	void getAllContactListsWithContact(Contact contact, AsyncCallback<Vector<ContactList>> callback);
-
-	void deletePermission(User user, BusinessObject bo, AsyncCallback<Void> callback);
-
-	void getAllContactsWithValue(String content, AsyncCallback<Vector<Contact>> callback);
-
-	void getAllContactsWithName(String name, AsyncCallback<Vector<Contact>> callback);
-
-	void getPropertyOfValue(Value value, AsyncCallback<Property> callback);
-
-	void getAllSharedValuesOfContact(Contact contact, AsyncCallback<Vector<Value>> callback);
-
-	void getAllPredefinedPropertiesOf(AsyncCallback<Vector<Property>> callback);
-
-	void createProperty(Contact contact, String type, AsyncCallback<Property> callback);
-
-	void getAllUsers(AsyncCallback<Vector<User>> callback);
-
-	void getPropertyByType(String type, Contact contact, AsyncCallback<Property> callback);
-
-	void getAllSharedContactsOfContactList(ContactList contactlist, User user, AsyncCallback<Vector<Contact>> callback);
-
-	void getUserByEmail(String email, AsyncCallback<User> callback);
-
-	void getAllContactsOfUserWithValue(User user, Value value, AsyncCallback<Vector<Contact>> callback);
-
-	void getContactsOfUserWithProperty(User user, Property Property, AsyncCallback<Vector<Contact>> callback);
-
-	void getContactsOfNameSearchResult(User user, String textBox, ContactList selectedContactList,
-			AsyncCallback<Vector<Contact>> callback);
-
-	void getContactsOfValueSearchResult(User user, String textBox, ContactList selectedContactList,
-			AsyncCallback<Vector<Contact>> callback);
-
-	void getAllSharedContactsOfUserWithOtherUser(User source, String receiver, AsyncCallback<Vector<Contact>> callback);
-
-	void getUserByID(int ID, AsyncCallback<User> callback);
-
-	void getSourceToSharedContact(Contact contact, User receivingUser, AsyncCallback<User> callback);
-
-	void getAllPermissions(AsyncCallback<Vector<Permission>> callback);
-
-	void getAllContactsOfContactlistForUser(ContactList contactlist, User user,
-			AsyncCallback<Vector<Contact>> callback);
-
-	void getOwnerOfContact(Contact c, AsyncCallback<User> callback);
-
-	void createAddress(String street, String housenumber, String zip, String city, Contact contact,
-			AsyncCallback<Value> callback);
-
-	void createUserContact(String firstname, String lastname, String sex, String email,
-			AsyncCallback<User> callback);
-
-	void getAllParticipantsOfContact(Contact contact, AsyncCallback<Vector<User>> callback);
-
-	void getAllUserSuggestions(User activeUser, AsyncCallback<Vector<String>> callback);
-
-	void getFullNamesOfUsers(Vector<User> user, AsyncCallback<Vector<String>> callback);
-
-	void getContactsOfUserWithDefaultProperty(User user, Property property, AsyncCallback<Vector<Contact>> callback);
-
-	void editProperty(Property property, AsyncCallback<Property> callback);
-
-	void deleteProperty(Property property, AsyncCallback<Void> callback);
-
-	void deleteUser(User u, AsyncCallback<Void> callback);
-
-	void editUser(User user, AsyncCallback<User> callback);
-
-	void editPermissionContact(Permission permission, AsyncCallback<Permission> callback);
-
-	void editPermissionContactList(Permission permission, AsyncCallback<Permission> callback);
-
-	void getAllParticipantsOfContactList(ContactList contactlist, AsyncCallback<Vector<User>> callback);
 }
-
